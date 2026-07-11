81194810504333325d47129ddd951150cf900d85
lennyluong <lennyluong2723@gmail.com>
2026-07-11 14:31:34 -0700

    Add database creation script

---
+++ sql/01_create_database.sql
@@ -0,0 +1,14 @@
+/*
+NBA Player and Team Performance Analysis
+File: 01_create_database.sql
+
+Purpose:
+Creates the database used for the NBA analytics project.
+
+Note:
+PostgreSQL does not allow CREATE DATABASE to run while connected
+to the same database. Run this command from the default postgres
+database or create the database manually through pgAdmin.
+*/
+
+CREATE DATABASE nba_analytics;
